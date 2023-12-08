876dfa71a9ff3cc523d659988fb1df9c0b6de352
unknown <calemamanispe.12@gmail.com>
2023-12-08 19:20:48 -0400

    N5 Destructuring

---
+++ destrucng.js
@@ -0,0 +1,16 @@
+const person = {
+    head: {
+        eyes: '8',
+        mouth: {
+            teeth: '7',
+            tongue: '4',
+        }
+    },
+    body: {
+        shoulders: '10',
+        chest: '9',
+        arms: '8.5',
+        hands: '9',
+        legs: '10',
+    }
+};
