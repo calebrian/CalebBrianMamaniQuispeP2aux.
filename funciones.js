d17af86315b9684c61890e7336163eb69065c6c3
unknown <calemamanispe.12@gmail.com>
2023-12-08 18:19:36 -0400

    Exercise N1 Functions

---
+++ funciones.js
@@ -0,0 +1,7 @@
+function square(numero) {
+    var cuadrado = numero * numero;
+    return cuadrado;
+    }
+    var numero = 5;
+    var resultado = square(numero);
+    console.log("el cuadrado de " + numero + " es "+ resultado);
