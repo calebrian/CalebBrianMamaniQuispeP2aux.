19756c1c1ad981bcab2f9b4cc24fa1f70727052a
unknown <calemamanispe.12@gmail.com>
2023-12-08 19:47:45 -0400

    N8 modulos

---
+++ modulos.js
@@ -0,0 +1,15 @@
+export const name = "Tu Nombre Completo";
+export const firstName = "Tu Primer Nombre";
+export const lastName = "Tu Apellido";
+export const age = TuEdad; // Reemplaza TuEdad con tu edad real
+
+export const career = "Tu Profesión";
+
+import { name, firstName, lastName, age } from './dates.js';
+import { career } from './profession.js';
+
+console.log(`Nombre completo: ${CalebMamaniQuispe}`);
+console.log(`Primer nombre: ${Caleb}`);
+console.log(`Apellido: ${Mamani}`);
+console.log(`Edad: ${age} 23`);
+console.log(`Profesión: ${universitario}`);
