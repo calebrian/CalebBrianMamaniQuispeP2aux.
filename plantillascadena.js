8450b3f3de3c13e40f5198616e4cbe53139e7b02
unknown <calemamanispe.12@gmail.com>
2023-12-08 19:42:44 -0400

    N6 Plantillascadena

---
+++ plantillascadena.js
@@ -0,0 +1,11 @@
+const Name = "Tu Nombre Completo";
+const FirstName = "Tu Primer Nombre";
+const LastName = "Tu Apellido";
+const Age = TuEdad;
+const hubby = "Tu Hobby";
+
+console.log(`Mi nombre completo es: ${CalebBrianMamaniQuispe}`);
+console.log(`Mi primer nombre es: ${Caleb}`);
+console.log(`Mi apellido es: ${Mamani}`);
+console.log(`Tengo ${Age} 23.`);
+console.log(`Mi hobby es: ${wali}`);
