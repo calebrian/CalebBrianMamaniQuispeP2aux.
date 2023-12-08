28ea13cc5ef30335a44bf01f7a4ba5e381afea34
unknown <calemamanispe.12@gmail.com>
2023-12-08 18:50:38 -0400

    N2 Arrays

---
+++ Arrays.js
@@ -0,0 +1,11 @@
+const Student = {
+    firstName: "Isaac Rodrigo",
+    lastName: "Zegarra Mamani",
+    age: 24,
+    hobby: "wali"
+};
+
+console.log(`Nombre: ${Student.firstName}`);
+console.log(`Apellido: ${Student.lastName}`);
+console.log(`Edad: ${Student.age}`);
+console.log(`Hobby: ${Student.hobby}`);
